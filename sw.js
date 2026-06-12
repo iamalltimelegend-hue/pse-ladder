cc0a3b7284571c381295017677b5c7421af6245e
김대환 <daehwankim@gimdaehwans-MacBook-Air.local>
2026-06-12 16:16:47 +0100

    PSE Ladder PWA

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'pse-ladder-v1';
+const FILES = ['./pse_ladder.html', './manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
